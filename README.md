5053e52e1d9c00cf949c367633813fd52df46bad
jamie han <iheart79@gmail.com>
2018-04-09 11:43:29 +0900

    c

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 
 [Chapter #3](https://docs.google.com/presentation/d/1rrNx7_zDwwm3w3sm8s5BCfySST3jG88L5Bi1qB8BSIw)
 
+[Chapter #4](https://docs.google.com/presentation/d/1gSf5f6PidQDd_Y_T0tRuCJB0XlYOPv6aIGzxgtS8s8Q/edit?usp=sharing)
--- workspace/c05/js/add-element.js
+++ workspace/c05/js/add-element.js
@@ -1,11 +1,11 @@
 // 새로운 요소를 생성하여 변수에 저장한다.
-var newEl = document.createElement('li');
+var newEl = document.createElement('li'); // <li></li>
 
 // 텍스트 노드를 생성한 후 변수에 저장한다.
-var newText = document.createTextNode('퀴노아');
+var newText = document.createTextNode('퀴노아');    // 퀴노아
 
 // 새로 생성한 텍스트 노드를 새 요소에 추가한다.
-newEl.appendChild(newText);
+newEl.appendChild(newText); // <li>퀴노아</li>
 
 // 새 요소가 추가될 위치를 선정한다.
 var position = document.getElementsByTagName('ul')[0];
